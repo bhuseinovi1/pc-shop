9fa97ec63fee915a15569b6f392e4f68942210ff
Belmin <bhuseinovi1@etf.unsa.ba>
2022-12-03 12:19:21 +0100

    implementirani moduli za produkte

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { IPagination } from './modules/pagination';
+import { IProduct } from './modules/product';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +11,7 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
 
   title = 'PC SHOP';
-  products: any[] | undefined;
+  products: IProduct[] | undefined;
 
   constructor(private http: HttpClient) {}
 
@@ -20,7 +22,7 @@ export class AppComponent implements OnInit {
       console.log(error);
     }) */
     this.http.get('https://localhost:5209/api/products?pageSize=50').subscribe({
-      next: (p : any) => {
+      next: (p : IPagination) => {
         this.products = p.data;
       },
       error: (e) => console.log(e),
--- client/src/demo.ts
+++ client/src/demo.ts
@@ -1,1 +1,22 @@
-let data = 100;
+let data : number | string;
+
+data = '42';
+
+data = 100;
+
+interface ICar {
+  color: string,
+  model: string
+}
+
+const car: ICar
+= {
+  color: 'blue',
+  model: 'bmw'
+};
+
+const multiply = (x,y) => {
+  return x*y;
+}
+
+
